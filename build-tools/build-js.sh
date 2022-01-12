017117d064b464aad2e5082912fdfdb2c821b01b
sspanak <doftor.livain@gmail.com>
2022-01-12 14:17:35 +0200

    отново използваме Babel за превод на javascript-a

--- build-tools/build-js.sh
+++ build-tools/build-js.sh
@@ -25,8 +25,7 @@ do
 	if $DEBUG ; then
 		mv dist/$PAGE_NAME/compiled.js dist/$PAGE_NAME/$PAGE_NAME.js
 	else
-		# npx babel dist/$PAGE_NAME/compiled.js | npx terser -c passes=2 > dist/$PAGE_NAME/$PAGE_NAME.js
-		cat dist/$PAGE_NAME/compiled.js | npx terser -c passes=2 > dist/$PAGE_NAME/$PAGE_NAME.js
+		npx babel dist/$PAGE_NAME/compiled.js | npx terser -c passes=2 > dist/$PAGE_NAME/$PAGE_NAME.js
 	fi
 
 	rm -f dist/$PAGE_NAME/compiled.js
