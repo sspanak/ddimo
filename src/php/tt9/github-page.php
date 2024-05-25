ca59093ccb18729eb475134f5b704a51c8299f37
sspanak <doftor.livain@gmail.com>
2024-05-25 15:05:14 +0300

    поддръжка на новите линкове за теглене на ТТ9

--- src/php/tt9/github-page.php
+++ src/php/tt9/github-page.php
@@ -42,9 +42,13 @@ class GithubPage {
 	public function get_install_section() {
 		$section = $this->get_section('@##[^\n]+Install\n([^#]+)@');
 
+		$section = preg_replace('@\s*(?:</?tr>|</?td>| {2,})@', '', $section);
+		$section = str_replace('<table>', '<div class="download-links">', $section);
+		$section = str_replace('</table>', '</div>', $section);
 		$section = preg_replace('@\[([^]]+)\]\(([^)]+)\)@', '<a href="$2">$1</a>', $section);
 		$section = str_replace('src="!RAW', 'src="'.self::GITHUB_BASE_URL.'/!RAW', $section);
-		$section = preg_replace('@\n\n@', '<br/>', $section);
+		$section = preg_replace('@\n\n([^\n]+?)\n@', '<p>$1</p>', $section);
+		$section = preg_replace('@_([^_]+?)_@', '<i>$1</i>', $section);
 		$section = str_replace('docs/installation.md', "installation", $section);
 		$section = is_text_browser() ? str_replace('></a>', '></a><br/>', $section) : $section;
 
